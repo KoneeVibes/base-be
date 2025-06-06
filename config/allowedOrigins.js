c1f090fee16d87d9ade1ec9ec491a27dbac9c7df
Konee <104918313+KoneeVibes@users.noreply.github.com>
2025-06-06 11:51:13 +0100

    adding more allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,9 @@ const allowedOrigins = [
     "http://localhost:3030",
     "http://localhost:8085",
     "http://localhost:3031",
+    "https://base-fe.onrender.com",
+    "https://base-be.onrender.com",
+    "https://designsbylami.onrender.com",
 ];
 
 module.exports = allowedOrigins;
