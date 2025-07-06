08b52a7ec8e33d0589122ccfdf3fc76c42b033f8
Konee <104918313+KoneeVibes@users.noreply.github.com>
2025-07-06 05:03:22 +0100

    added more allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,12 +1,14 @@
 const allowedOrigins = [
     "http://192.168.0.152:3030",
     "http://localhost:3030",
+    "http://localhost:3000",
     "http://localhost:8085",
     "http://localhost:3031",
     "https://base-fe.onrender.com",
     "https://base-be.onrender.com",
     "https://designsbylami.onrender.com",
-    "https://www.designsbylam.com"
+    "https://www.designsbylam.com",
+    "https://www.valleyridgeenergyservices.com",
 ];
 
 module.exports = allowedOrigins;
