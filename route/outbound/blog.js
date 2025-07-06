c3fb8d07bdc61fbaf9951c7ded98dd7022a6534c
Konee <104918313+KoneeVibes@users.noreply.github.com>
2025-07-06 09:34:03 +0100

    added another route to retrieve single blog information

--- route/outbound/blog.js
+++ route/outbound/blog.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.get("/", require("../../controller/outbound/blog/retrieveallblog"));
 router.get("/:projectId", require("../../controller/outbound/blog/retrieveallprojectblog"));
+router.get("/:projectId/:blogId", require("../../controller/outbound/blog/retrieveblog"));
 
 module.exports = router;
